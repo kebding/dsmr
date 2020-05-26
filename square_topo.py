44bbcd9b6ccb8275358c8e98c1206a01beb0e79b
kebding <kebding@gmail.com>
2020-05-26 11:45:32 -0700

    add comments to square_topo.py

--- square_topo.py
+++ square_topo.py
@@ -6,7 +6,7 @@ from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
 from mininet.node import RemoteController
-from mininet.link import TCLink
+from mininet.link import TCLink, Intf
 
 ''' 
 launches a custom square topology that will have two routes from s1 to s3 when
@@ -15,21 +15,23 @@ using multipath_dijkstra due to the differences in bandwidth across the links
 '''
 class square_topo(Topo):
   def build(self):
+    # create the hosts and switches
     s1 = self.addSwitch('s1')
     s2 = self.addSwitch('s2')
     s3 = self.addSwitch('s3')
     s4 = self.addSwitch('s4')
-
     h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='10.0.0.1/24')
     h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='10.0.0.2/24')
     h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='10.0.0.3/24')
     h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='10.0.0.4/24')
+
+    # create the host-to-switch links
     self.addLink(h1,s1)
     self.addLink(h2,s2)
     self.addLink(h3,s3)
     self.addLink(h4,s4)
-    
-    
+
+    # create the switch-to-switch links
     self.addLink(s1, s2, bw=30)
     self.addLink(s1, s3, bw=10)
     self.addLink(s3, s4, bw=15)
